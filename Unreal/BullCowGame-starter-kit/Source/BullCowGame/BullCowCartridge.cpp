d913b64e0e73fcd3d50793e6429107cf2f1f05c3
Delta0077 <adas0262@gmail.com>
2021-01-11 00:22:55 +0530

    Update BullCowCartridge.cpp

--- Unreal/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ Unreal/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -1,10 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
-FString HiddenWord = TEXT("cake"); //Move outside this function
+ //Move outside this function
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
+    HiddenWord = TEXT("cake");
     Super::BeginPlay();
     PrintLine(TEXT("Welcome to Bull Cow game...."));
     PrintLine(TEXT("Press Enter to continue...."));
