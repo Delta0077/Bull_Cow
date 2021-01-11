c970b8c8123077c83d27b488fb7517cdfbef45bf
Delta0077 <adas0262@gmail.com>
2021-01-11 22:28:34 +0530

    Update BullCowCartridge.cpp

--- Unreal/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ Unreal/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
- //Move outside this function
+ 
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
@@ -13,9 +13,8 @@ void UBullCowCartridge::BeginPlay() // When the game starts
     PrintLine(TEXT("Press Enter to continue...."));
     PrintLine(TEXT("Enter Your word..."));
 
-    // Setting Up Game
-    HiddenWord = TEXT("cake");  // Set the HiddenWord
-    Lives = 4; // Set Lives
+    SetupGame();
+    
 
     // Prompt Player Guess
 }
@@ -23,6 +22,8 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
         ClearScreen();
+
+        HiddenWord.Len();
         
         // Checking PlayerGuess
         
@@ -32,11 +33,13 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
         }
         else
         {
+             if (Input.Len() != HiddenWord.Len())
+            {
+            PrintLine(TEXT("The Hidden Word is 4 characters long, try again!"));
+            }
             PrintLine(TEXT("Wrong Word try again!"));
-            
-            
         }
-        
+       
         // Check If Isogram
         // Prompt to Guess Again
         // Check Right Number of Characters
@@ -54,3 +57,9 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
 
         
 }
+
+        void UBullCowCartridge::SetupGame()
+        {
+            HiddenWord = TEXT("cake");  // Set the HiddenWord
+            Lives = 4; // Set Lives
+        }
