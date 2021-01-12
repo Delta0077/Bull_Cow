461270899d6b0e917dbe58925d6bfd07b3111866
Delta0077 <adas0262@gmail.com>
2021-01-13 01:44:43 +0530

    Update BullCowCartridge.h

--- Unreal/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.h
+++ Unreal/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.h
@@ -17,11 +17,12 @@ class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 	void SetupGame();
 	void EndGame();
 	void ProcessGuess(FString Guess);
+	bool bIsIsogram();
 
 	// Your declarations go below!
 	private:
 	FString HiddenWord;
 	int32 Lives;
 	bool bGameOver;
+	
 };
-
